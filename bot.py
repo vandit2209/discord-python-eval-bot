ab1290c1e65da29278c4e5b73e3486b487ff5fae
aravk33 <aravkasi@gmail.com>
2020-06-20 18:03:40 +0530

    Fix code and output format

--- bot.py
+++ bot.py
@@ -12,21 +12,23 @@ async def on_ready():
 
 
 @bot.command(aliases=["eval"])
-async def evaluate(ctx, *, code):
-    if match := re.fullmatch(r"`(?:``(?:py(?:thon)?)?\n((?:.|\n)*)\n``|(.*))`", code):
-        code = match.group(1)
+async def evaluate(ctx, *, command):
+    if match := re.fullmatch(r"(?:\n*)?`(?:``(?:py(?:thon)?\n)?((?:.|\n)*)``|(.*))`", command, re.DOTALL):
+        code = match.group(1) if match.group(1) else match.group(2)
         str_obj = io.StringIO()  # Retrieves a stream of data
         try:
             with contextlib.redirect_stdout(str_obj):
                 exec(code)
         except Exception as e:
-            return await ctx.send(f"""```
-                ❌ Your code was completed with execution code 1
-                {e.__class__.__name__}: {e}```""")
-        return await ctx.send(f"""```
-            ✅ Your code was completed with execution code 0
-            {str_obj.getvalue()}```""")
+            return await ctx.send(f"""❌ Your code completed with execution code 1
+```
+{e.__class__.__name__}: {e}
+```""")
+        return await ctx.send(f"""✅ Your code completed with execution code 0
+```
+{str_obj.getvalue()}
+```""")
     return await ctx.send('Invalid format')
 
 
-bot.run("Your token here")
+bot.run("Token here")
